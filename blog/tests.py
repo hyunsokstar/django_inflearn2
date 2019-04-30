df37b9832c3fc77d91a886e92604d26faf614e7d
hyun2 <dktmvktm@daum.net>
2019-04-30 12:41:24 +0900

    added category model and category filed for Post model and test code

--- blog/tests.py
+++ blog/tests.py
@@ -1,20 +1,49 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post, Category
+
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-
 # Create your tests here.
 
-def create_post(title, content, author):
+def create_category(name='life', description=''):
+    category, is_created = Category.objects.get_or_create(
+        name=name,
+        description=description
+    )
+
+    return category
+
+def create_post(title, content, author, category=None):
     blog_post = Post.objects.create(
         title=title,
         content=content,
         created=timezone.now(),
-        author=author
+        author=author,
+        category=category
     )
     return blog_post
+
+# Test Model
+class TestModel(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='smith', password='nopassword')
+
+    def test_category(self):
+        category = create_category()
+
+    def test_post(self):
+        category = create_category()
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We are the world.',
+            author=self.author_000,
+            category=category
+        )
+
 
 # 테스트 뷰
 class TestView(TestCase):
