ee3b25ba4928b13e83d4a400f211052d89ad99b2
hyun2 <dktmvktm@daum.net>
2019-05-01 20:16:07 +0900

    test_post_detail_about_output_of_categoryInfo_and_editButton

--- blog/tests.py
+++ blog/tests.py
@@ -40,7 +40,12 @@ def create_post(title, content, author, category=None):
 class TestModel(TestCase):
     def setUp(self):
         self.client = Client()
-        self.author_000 = User.objects.create(username='smith', password='nopassword')
+        # self.author_000 = User.objects.create_user(username='smith', password='nopassword')
+        self.author_000 = User.objects.create(username='smith')
+        self.author_000.set_password('1234')
+        self.author_000.save()
+
+        # self.user_obama = User.objects.create_user(username='obama', password='1234')
 
     def test_category(self):
         category = create_category()
@@ -96,8 +101,15 @@ class TestModel(TestCase):
 # 테스트 뷰
 class TestView(TestCase):
     def setUp(self):
+        # print("setup 실행")
         self.client = Client()
-        self.author_000 = User.objects.create(username='smith', password='nopassword')
+        self.author_000 = User.objects.create(username='smith')
+        self.author_000.set_password('1234')
+        self.author_000.save()
+
+        self.author_001 = User.objects.create(username='obama')
+        self.author_001.set_password('1234')
+        self.author_001.save()
 
     def check_navbar(self, soup):
         navbar = soup.find('div', id='navbar')
@@ -184,7 +196,7 @@ class TestView(TestCase):
         # post 입력이 제대로 되었는지 확인(게시글이 0개보다 많은지 테스트)
         self.assertGreater(Post.objects.count(), 0)
 
-        # post 객체.getAbsoulte()의 결과값이 예상대로 상세보기 url 인지 확인
+        # post 객체.Absoulte()의 결과값이 예상대로 상세보기 url 인지 확인
         post_000_url = post_000.get_absolute_url()
         self.assertEqual(post_000_url, '/blog/{}/'.format(post_000.pk))
 
@@ -297,8 +309,120 @@ class TestView(TestCase):
         # 그 태그의 text로 blog_000.text 즉 클릭한 태그 이름이 출력되는지 확인
         blog_h1 = main_div.find('h1', id='blog-list-title')
         self.assertIn('#{}'.format(tag_000.name), blog_h1.text)
-        # 
+        #
         # 해당 태그 정보(클릭한 태그)에 대한 posting은 출력
         # 아닌 posting은 출력되면 안됨
         self.assertIn(post_000.title, main_div.text)
         self.assertNotIn(post_001.title, main_div.text)
+
+    def test_post_list_with_post(self):
+        # post 모델 입력
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We are the world.',
+            author=self.author_000,
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Second Second Second',
+            author=self.author_000,
+            category=create_category(name='정치/사회')
+        )
+
+        # post_list_view 요청 하기
+        response = self.client.get('/blog/')
+        # 응답 텍스트를 가져오기 위해  soup 객체 생성
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # soup 객체를 이용해 body 객체 생성
+        body = soup.body
+        # soup 객체를 이용해 title 태그 객체 생성
+        title = soup.title
+        # title이 'Blog로 출력되는지 확인'
+        self.assertEqual(title.text, 'Blog')
+        # nav bar 가 있어야 한다.
+        self.check_navbar(soup)
+        # Post 모델 개수 확인 하기(0개 이상)
+        self.assertGreater(Post.objects.count(), 0)
+        # 아직 아직 게시물이 없습니다 문자열이 없어야 한다.
+        self.assertNotIn('아직 게시물이 없습니다', body.text)
+
+        # 오른쪽 카테고리 메뉴의 항목이 제대로 출력되는지 테스트
+        self.check_right_side(soup)
+
+        # main_div에는
+        main_div = soup.find('div', id='main-div')
+        self.assertIn('정치/사회', main_div.text)  # '정치/사회' 있어야 함
+        self.assertIn('미분류', main_div.text)  # '미분류' 있어야 함
+
+    # post_detail page 출력시에
+    # 카테고리 정보 태그 정보에디트 버튼 출력에 대해 테스트
+    def test_post_detail_about_output_of_categoryInfo_and_editButton(self):
+        # Post 모델에 row 데이터 1개 입력
+        category_politics = create_category(name='정치/사회')
+
+        # Post 모델에 데이터 2개 입력
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We are the world.',
+            author=self.author_000,
+            category=category_politics
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Second Second Second',
+            author=self.author_000,
+            category=create_category(name='취미/생활')
+        )
+
+        # post_000에 태그 정보 추가
+        tag_america = create_tag(name='america')
+        post_000.tags.add(tag_america)
+        post_000.save()
+
+        # 상세 보기 url 가져오기
+        post_000_url = post_000.get_absolute_url()
+        # 상세 보기 url 요청
+        response = self.client.get(post_000_url)
+        # 응답 완료 확인
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        body = soup.body
+        main_div = body.find('div', id='main-div')
+        # 카테 고리 정보는 출력
+        self.assertIn(category_politics.name, main_div.text)
+        # edit 버튼은 미출력
+        self.assertNotIn('EDIT', main_div.text)
+        # tags 정보 출력 확인
+        self.assertIn('#america', main_div.text)
+        # print("유저 : " ,User.objects.all())
+
+        # login을 한 경우,
+        login_success = self.client.login(username='smith', password='1234')
+        self.assertTrue(login_success)
+
+        response = self.client.get(post_000_url)
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_div = soup.find('div', id='main-div')
+        # print("post_000.author : ", post_000.author)
+        # print("self.author_000 : ", self.author_000)
+
+        self.assertEqual(post_000.author, self.author_000)
+
+        # EDIT 버튼이 있다.
+        self.assertIn('EDIT', main_div.text)
+
+        # 다른 사람인 경우에는 없다.
+        login_success = self.client.login(username='obama', password='1234')
+        self.assertTrue(login_success)
+
+        response = self.client.get(post_000_url)
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_div = soup.find('div', id='main-div')
+
+        self.assertEqual(post_000.author, self.author_000)
+        self.assertNotIn('EDIT', main_div.text)
