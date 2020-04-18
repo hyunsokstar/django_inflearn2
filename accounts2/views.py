a2a9737a07b48d5e7d4c81c2b255ad8b30d9a8cf
hyun2 <dktmvktm@daum.net>
2020-04-18 17:06:47 +0900

    update for boiler-plate

--- accounts2/views.py
+++ accounts2/views.py
@@ -6,14 +6,16 @@ from django.contrib.auth import login as auth_login
 
 
 def signup(request):
-    print('회원 가입 뷰 실행')
+    print('회원 가입 뷰 실행 22')
     if request.method=="POST":
         form = SignupForm(request.POST)
         if form.is_valid():    # 입력한 값이 있을 경우 True를 반환
             user = form.save()
 
             auth_login(request, user, backend = 'django.contrib.auth.backends.ModelBackend')
-            return redirect(settings.LOGIN_URL)
+            redirect_url = request.GET.get("next",settings.LOGIN_REDIRECT_URL)
+
+            return redirect(redirect_url)
 
     else:   # 입력한 값이 없을 경우
         form = SignupForm()
