9e98ca665d835f3cd04b060d27e04500f5749bef
hyun2 <dktmvktm@daum.net>
2020-07-27 13:31:52 +0900

    update for skilnote2 before

--- wm/urls.py
+++ wm/urls.py
@@ -85,13 +85,13 @@ urlpatterns = [
     path('myshortcut/update/shortcut_subject/' , views.update_my_shortcut_subject, name="update_my_shortcut_subject"),
 
     path('myshortcut/create_new1_input/ajax/first', views.create_new1_input_first , name="create_new1_input_first"),
+    path('myshortcut/create_new2_textarea_first/ajax/', views.create_new2_textarea_first , name="create_new2_textarea_first"),
+    path('myshortcut/create_summernote_first/ajax/', views.create_summernote_first , name="create_summernote_first"),
     path('myshortcut/create_new1_input/ajax/', views.create_new1_input , name="create_new1_input"),
 
     path('myshortcut/create_new1_input_between/ajax/<int:current_article_id>', views.create_new1_input_between , name="create_new1_input_between"),
 
     path('myshortcut/create_new2_textarea/ajax/', views.create_new2_textarea , name="create_new2_textareas"),
-    path('myshortcut/create_new2_textarea_first/ajax/', views.create_new2_textarea_first , name="create_new2_textarea_first"),
-    path('myshortcut/create_summernote_first/ajax/', views.create_summernote_first , name="create_summernote_first"),
 
 
 
