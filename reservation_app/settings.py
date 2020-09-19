1707ecf43ad12bc3571edbf62ef7919c8738157f
unknown <hyunsokstar@google.com>
2020-09-19 17:51:07 +0900

    update for java-rest-api-basic

--- reservation_app/settings.py
+++ reservation_app/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'markdownx',
+    # 'markdownx',
     'crispy_forms',
     'easy_thumbnails',
     'remote_control',
